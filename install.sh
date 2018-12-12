3756093b3779b6d6ab0fae3082106d67a3e9382d
Tim Savannah <kata198@gmail.com>
2018-12-11 21:47:45 -0500

    install.sh - Major rework. Install the README.md and LICENSE in /usr/share/ioschedset and install the LICENSE into /usr/share/licenses/ioschedset. Create functions 'ensure_directory' and 'install_file' to perform those tasks and replace the previously unrolled actions with calls to these standardized functions. Fix a few of the calls to 'die' missing the exit code requested in the format string. Ensure we pass explicit exit code in all calls to die now, but modify 'die' such that if the first arg (Exit code) is not a number, assume that is format string (arg 2) and assign exit code to 1.

--- install.sh
+++ install.sh
@@ -66,18 +66,88 @@ EOT
 #    3rd..Nth arg - Format string args - Any va_args to resolve
 #                     within your format string
 #
+#  NOTE: If you forget to specify the exit code 
+#    and just start up with the format string, it will
+#    assume exit code = 1
+#
 ############################################
 die() {
-
+    
     EXIT_CODE=$1
     shift;
+    if ( echo "${EXIT_CODE}" | grep -Eq '^[0-9][0-9]*$' );
+    then
+        # First arg was a number, so keep going
+        FORMAT_STR="$1"
+        shift
+    else
+        # Oops! Mising exit code, assume one.
+        FORMAT_STR="${EXIT_CODE}"
+        EXIT_CODE=1
+    fi
 
-    FORMAT_STR="$1"
-    shift
-
-    printf "${FORMAT_STR}" "$@" >&2
+    printf "${FORMAT_STR}\n" "$@" >&2
     
     exit ${EXIT_CODE}
+}
+
+
+########################
+# 
+# ensure_dir - Ensure a directory exists at the given location
+#
+#    1st arg - Path to ensure
+#
+#  If directory does not exist and fails to be created,
+#    a warning message will be output on stderr
+#    and non-zero return.
+#
+#  Otherwise, 0 return if directory already present
+#   or we sucessfully created it
+#
+############################################
+ensure_dir() {
+    _ENSURE_DIR="$1"
+
+    [[ -d "${_ENSURE_DIR}" ]] && return 0;
+
+    printf "\tmkdir -p \"%s\"\n" "${_ENSURE_DIR}"
+    mkdir -p "${_ENSURE_DIR}"
+    RET=$?
+    if [[ $RET -ne 0 ]];
+    then
+        printf "  WARNING: Could not create directory \"%s\", install may fail. Check permissions?\n\n" "${_ENSURE_DIR}" >&2
+    fi
+
+    return $RET
+}
+
+install_file() {
+
+    FROM_PATH="$1"
+    # Exit code 22 = EINVAL = Invalid argument
+    [[ -z "${FROM_PATH}" ]] && die 22 'Missing 1st argument, "from path" to install_file'
+    shift
+
+    # Exit code 2 = ENOENT = No such file or directory
+    [[ ! -e "${FROM_PATH}" ]] && die 2 "Tried to install \"${FROM_PATH}\" but file does not exist."
+
+    TO_PATH="$1"
+    [[ -z "${TO_PATH}" ]] && die 22 'Missing 2nd argument, "to path" to install_file'
+    shift
+
+    MODE="$1"
+    [[ -z "${MODE}" ]] && die 22 'Missing 3rd argument, "mode" to install_file'
+
+
+    printf "\t"'install -m "%s" "%s" "%s"'"\n" "${MODE}" "${FROM_PATH}" "${TO_PATH}"
+    install -m "${MODE}" "${FROM_PATH}" "${TO_PATH}"
+    RET=$?
+    [ ${RET} -ne 0 ] && die ${RET} "  Failed to install \"%s\" to \"%s\".  Exit code=%d\n\nAborting...\n" "${FROM_PATH}" "${TO_PATH}" ${RET}
+
+
+    # This will never be non-zero because we will "die" above
+    return ${RET}
 }
 
 
@@ -115,11 +185,12 @@ printf "DESTDIR=%s\nPREFIX=%s\n\nInstalling into: %s\n\n" "${DESTDIR}" "${PREFIX
 ##         INSTALL EXECUTABLES         ##
 #########################################
 
-mkdir -p "${INSTALLDIR}/bin" || printf "  WARNING: Could not create directory '${INSTALLDIR}/bin', install may fail. Check permissions?\n\n" >&2
+printf "** INSTALLING EXECUTABLES **\n-----------------------------\n\n"
+
+ensure_dir "${INSTALLDIR}/bin"
 
 # Install io-get-sched
-printf "\t"'install -m 755 io-get-sched "'"${INSTALLDIR}"'/bin"'"\n"
-install -m 755 io-get-sched "${INSTALLDIR}/bin" || die $? "  Failed to install \"%s\" to \"%s\". Exit code=%d\n\nAborting...\n" 'io-get-sched' "${INSTALLDIR}/bin" "$?" 
+install_file 'io-get-sched' "${INSTALLDIR}/bin" 755
 cat <<EOT
 	sed -e 's/^VERSION=.*$/VERSION=${VERSION}/g' -i "${INSTALLDIR}/bin/io-get-sched"
 EOT
@@ -128,8 +199,7 @@ sed -e 's/^VERSION=.*$/VERSION='"${VERSION}"'/g' -i "${INSTALLDIR}/bin/io-get-sc
 echo;
 
 # Install io-set-sched
-printf "\t"'install -m 755 io-set-sched "'"${INSTALLDIR}"'/bin"'"\n"
-install -m 755 io-set-sched "${INSTALLDIR}/bin" || die $? "  Failed to install \"%s\" to \"%s\". Exit code=%d\n\nAborting...\n" 'io-set-sched' "${INSTALLDIR}/bin" "$?" 
+install_file 'io-set-sched' "${INSTALLDIR}/bin" 755
 cat << EOT
 	sed -e 's/^VERSION=.*$/VERSION=${VERSION}/g' -i "${INSTALLDIR}/bin/io-set-sched"
 EOT
@@ -139,7 +209,9 @@ sed -e 's/^VERSION=.*$/VERSION='"${VERSION}"'/g' -i "${INSTALLDIR}/bin/io-set-sc
 ##         INSTALL MAN PAGES           ##
 #########################################
 
-mkdir -p "${INSTALLDIR}/share/man/man8" || printf "  WARNING: Could not create directory '${INSTALLDIR}/share/man/man3', install may fail. Check permissions?\n\n" >&2
+printf "\n\n** INSTALLING MAN PAGES **\n-------------------------\n"
+
+ensure_dir "${INSTALLDIR}/share/man/man8"
 
 # Install man pages
 for manpageName in "io-get-sched.8" "io-set-sched.8";
@@ -148,13 +220,32 @@ do
     printf "\t"'cat "man/'"${manpageName}"'" | gzip -c > "man/'"${manpageName}"'.gz"'"\n"
     (cat "man/${manpageName}" | gzip -c > "man/${manpageName}.gz") || die $? "  Failed to compress man page at \"man/${manpageName}\". Exit code=%d\n\nAborting...\n" $?
 
-
-    printf "\t"'install -m 755 "man/'"${manpageName}"'" "'"${INSTALLDIR}"'/share/man/man8'"\n"
-    install -m 755 "man/${manpageName}" "${INSTALLDIR}/share/man/man8" || die $? "  Failed to install \"%s\" to \"%s\".  Exit code=%d\n\nAborting...\n" "man/${manpageName}" "${INSTALLDIR}/share/man/man8"
+    install_file "man/${manpageName}.gz" "${INSTALLDIR}/share/man/man8" 644
 done
 
+#########################################
+##         INSTALL MISC SHARE          ##
+#########################################
+
+printf "\n\n** INSTALLING SHARE MISC **\n--------------------------\n"
+echo;
+# Install README and LICENSE
+ensure_dir "${INSTALLDIR}/share/ioschedset"
+
+install_file "README.md" "${INSTALLDIR}/share/ioschedset" 644
+install_file "LICENSE" "${INSTALLDIR}/share/ioschedset" 644
+
+echo;
+
+ensure_dir "${INSTALLDIR}/share/licenses/ioschedset"
+
+install_file "LICENSE" "${INSTALLDIR}/share/licenses/ioschedset" 644
 
 echo
+
+
+printf "\n** ALL DONE! Perfect A+ 11/10 Success on the install!\n\n"
+
 true
 
 # vim: set ts=4 sw=4 st=4 expandtab :
